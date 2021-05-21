29be9744976f3c2811beffa9737b10685bec231c
Wlad <wladkrychin@gmail.com>
2021-05-21 20:39:44 +0200

    1

--- hw-js/HW-4.js
+++ hw-js/HW-4.js
@@ -4,7 +4,6 @@ const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
 const counter = document.querySelector('#value')
 const removeBtn = document.querySelector('[data-action="remove"]')
-
 decrementBtn.addEventListener('click', () => {
     counter.textContent = counterValue -=1
 });
